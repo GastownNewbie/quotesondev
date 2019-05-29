076b874cfc3575b9a0c0ed0aae10af60e03cd761
Gay Stephenson <gaystephenson@me.com>
2019-05-29 16:37:19 -0700

    work on home page mobile

--- index.php
+++ index.php
@@ -7,21 +7,22 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
+		<?php if (have_posts()) : ?>
+
+			<?php if (is_home() && !is_front_page()) : ?>
 				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					<h1 class=" page-title screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
 			<?php endif; ?>
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php while (have_posts()) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+				<?php get_template_part('template-parts/content'); ?>
 
 			<?php endwhile; ?>
 
@@ -29,11 +30,15 @@ get_header(); ?>
 
 		<?php else : ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<?php get_template_part('template-parts/content', 'none'); ?>
 
 		<?php endif; ?>
+	</main><!-- #main -->
+	<nav id="site-navigation" class="main-navigation" role="navigation">
+		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html('Primary Menu'); ?></button>
+		<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
+	</nav><!-- #site-navigation -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
