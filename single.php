2e09723ad2cd84eae501c6b5452d2b3a364a7b96
Gay Stephenson <gaystephenson@me.com>
2019-06-06 16:46:06 -0700

    update single php to include button using home content

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all single posts.
+ * The template for displaying single posts (from home to include button).
  *
  * @package QOD_Starter_Theme
  */
@@ -10,15 +10,32 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
-		<?php while (have_posts()) : the_post(); ?>
 
-			<?php get_template_part('template-parts/content', 'single'); ?>
+		<?php if (have_posts()) : ?>
+
+			<?php if (is_home() && !is_front_page()) : ?>
+				<header>
+					<h1 class=" page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
+			<div class="home-content">
+				<?php /* Start the Loop */ ?>
+				<?php while (have_posts()) : the_post(); ?>
+
+					<?php get_template_part('template-parts/content'); ?>
+
+				<?php endwhile; ?>
 
 
-		<?php endwhile;
-	?>
 
+			<?php else : ?>
+
+				<?php get_template_part('template-parts/content', 'none'); ?>
+
+			<?php endif; ?>
+		</div>
 	</main><!-- #main -->
+
 </div><!-- #primary -->
 
 <?php get_footer(); ?>
